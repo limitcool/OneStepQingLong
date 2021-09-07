a228f400c8516600b6530a01e5f061ae913ddf7e
limitcool <initcoool@gmail.com>
2021-09-07 15:27:56 +0800

    commit

--- install.sh
+++ install.sh
@@ -18,22 +18,23 @@ os_() {
 		systemctl enable docker
 		systemctl start docker
 	fi
-	if command -v docker-compose >/dev/null 2>&1; then
-		echo "docker-compose已安装"
-	else
-		echo 'docker-compose 未安装'
-		echo '开始安装Docker-compose....'
-		# pip install --upgrade pip
-		# pip install docker-compose
-		curl -L "https://github.com/docker/compose/releases/download/v2.0.0-rc.3/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-		chmod +x /usr/local/bin/docker-compose
-	fi
 }
 if command -v apt >/dev/null 2>&1; then
 	os_ apt
 else
 	yum install -y yum-utils
 	os_ yum
+fi
+
+if command -v docker-compose >/dev/null 2>&1; then
+	echo "docker-compose已安装"
+else
+	echo 'docker-compose 未安装'
+	echo '开始安装Docker-compose....'
+	# pip install --upgrade pip
+	# pip install docker-compose
+	curl -L "https://github.com/docker/compose/releases/download/v2.0.0-rc.3/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+	chmod +x /usr/local/bin/docker-compose
 fi
 echo "############开始安装青龙##############"
 mkdir qinglong
